92859fb0a6d9dddc148a609370b080a30a34a865
JitendraVaid <144218847+jitendravaid8@users.noreply.github.com>
2023-09-07 14:28:25 +0200

    Add files via upload

---
+++ calcs.py
@@ -0,0 +1,23 @@
+class Calcs:
+	def __init__(self):
+		self.a =a
+		self.b =b
+
+	def adds(a,b):
+		result = a+b
+		return result
+
+	def subs(a,b):
+		result = a-b
+		return result
+
+	def muls(a,b):
+		result = a*b
+		return result
+
+	def divs(a,b):
+		result = a/b
+		return result
+
+if __name__ == "__main__":
+	print(result)
